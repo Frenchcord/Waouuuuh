3f1e3ff5c7b977ab1f081c638938e5a662e551aa
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-11 11:01:52 -0400

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,6 +1,8 @@
 from nextcord.ext import commands
 from ping_message import ping_message
-
+from utils import add_role
+import requests
+from os import environ
 class Ready(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
@@ -10,15 +12,17 @@ class Ready(commands.Cog):
     print('online')
     guild = self.bot.get_guild(978859818511110154)
     await guild.get_channel(984958731693482065).purge(limit=2)
-    role = guild.get_role(978860094932529214)
     for member in guild.members:
-      if 978860094932529214 not in [role.id for role in member.roles] and member.bot is False: await member.add_roles(role)
-    await guild.get_channel(985020834320105522).send('Jeg er pålogget') # norwegian ❤️
+      if 978860094932529214 not in [role.id for role in member.roles] and member.bot is False: add_role(978859818511110154, member.id, 978860094932529214)
+    payload: dict = {
+      'content': 'Jeg er pålogget'
+    }
+    requests.post(f'https://discord.com/api/v10/channels/985020834320105522/messages', headers={'authorization': 'Bot ' + environ['token']}, data=payload)
     await ping_message(self.bot)
 
   @commands.Cog.listener('on_member_join')
   async def join(self, member):
-    await member.add_roles(member.guild.get_role(978860094932529214))
+    add_role(978859818511110154, member.id, 978860094932529214)
 
 def setup(bot):
   bot.add_cog(Ready(bot))
