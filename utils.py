fdc8dd687c50028609a907da3ea6d49c99422569
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-11 11:01:07 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+import requests
+from os import environ
+url: str = 'https://discord.com/api/v10'
+def add_role(guild_id, member_id, role_id):
+  requests.put(f'{url}/guilds/{guild_id}/members/{member_id}/roles/{role_id}', headers={'authorization': 'Bot ' + environ['token']})
+def rem_role(guild_id, member_id, role_id):
+  requests.delete(f'{url}/guilds/{guild_id}/members/{member_id}/roles/{role_id}', headers={'authorization': 'Bot ' + environ['token']})
