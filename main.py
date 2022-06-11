3a8d5d94fcef18552ce7dfc09f2e0b56a4d17d95
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-11 10:17:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from nextcord.ext import commands; from nextcord import Intents; bot = commands.Bot(command_prefix=['?'], intents=Intents.all(), help_command=None); from os import listdir, environ
+from nextcord.ext import commands; from nextcord import Intents
+from os import listdir, environ
+bot = commands.Bot(command_prefix=['?'], intents=Intents.all(), help_command=None)
 for fn in listdir('cogs'):  bot.load_extension(f'cogs.{fn[: -3]}') if fn.endswith('.py') else print('not a python file')
 bot.run(environ['token'])
