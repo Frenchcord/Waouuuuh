b2ce565cb0f4baac9f4f133745abba1c106b6f3d
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-18 16:55:14 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ def add_role(guild_id, member_id, role_id):
   requests.put(f'{url}/guilds/{guild_id}/members/{member_id}/roles/{role_id}', headers={'authorization': 'Bot ' + environ['token']})
 def rem_role(guild_id, member_id, role_id):
   requests.delete(f'{url}/guilds/{guild_id}/members/{member_id}/roles/{role_id}', headers={'authorization': 'Bot ' + environ['token']})
+
+async def namesend(bot, content: str, channel):
+  webook = await channel.create_webhook(name='Name')
+  name = await bot.fetch_user(884220029867003916)
+  await webook.send(content, avatar_url=name.avatar.url)
+  await webook.delete()
